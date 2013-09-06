b01e3183b7bbd74e09f07ee0470f3526441eb224
Gastón <gaston@dc.exa.unrc.edu.ar>
2013-09-06 16:49:06 -0300

    changes in gramar and Main

--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import org.jgraph.graph.DefaultEdge;
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.graph.SimpleGraph;
+
+import parser.ProgramParser;
+
+
 
 public class Main {
 
@@ -6,6 +18,27 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		File inputScenarioFile = new File(args[0]);
+		FileReader fr;
+		ProgramParser scenarioParser;
+		try {
+			UndirectedGraph<String,DefaultEdge> graph = new SimpleGraph<String,DefaultEdge>(DefaultEdge.class); 
+			fr = new FileReader(inputScenarioFile);
+			scenarioParser = new ProgramParser(fr);
+			ProgramParser.init(fr);
+			scenarioParser.parseProgram(graph);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (parser.ParseException e) {
+			e.printStackTrace();
+			System.out.println("error in parsing scenario !!!");
+		}
+		
+		
+		
 
 	}
 

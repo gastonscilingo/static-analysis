95bc36d5e21e781ddf04ddcc40ced36cd26a2fe1
Gastón <gaston@dc.exa.unrc.edu.ar>
2013-10-11 15:51:16 -0300

    changes in main

--- Main.java
+++ Main.java
@@ -6,9 +6,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Set;
 
 
 import org.jgraph.graph.DefaultEdge;
+import org.jgrapht.alg.ConnectivityInspector;
 import org.jgrapht.graph.SimpleDirectedGraph;
 
 import algorithms.AlgorithmsDominators;
@@ -24,9 +26,7 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) throws IOException, InterruptedException {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) throws IOException, InterruptedException {		
 		
 		File inputScenarioFile = new File(args[0]);
 		FileReader fr;
@@ -34,26 +34,16 @@ public class Main {
 		SimpleDirectedGraph<Vertex,Edge> graph;
 		StringBuffer dotFile;
 		try {
-			//graph = new SimpleDirectedGraph<String,Edge>(Edge.class);
 			OwnEdgeFactory<Vertex,Edge> f = new OwnEdgeFactory<Vertex,Edge>(Edge.class);
 			graph = new SimpleDirectedGraph<Vertex,Edge>(f);
 			fr = new FileReader(inputScenarioFile);
 			scenarioParser = new ProgramParser(fr);
 			ProgramParser.init(fr);
 			
+			// Parse program and write CFG in dot file
 			dotFile = new StringBuffer("digraph name {\n");
 			scenarioParser.parseProgram(graph,dotFile);
 			dotFile.append("}\n");
-			
-			
-			// Print graph
-			System.out.println("\n	Graph : \n");
-			System.out.println("Vertex : "+graph.vertexSet().toString());
-			System.out.println("Edges : "+graph.edgeSet().toString());
-			
-			System.out.println(graph.toString());
-			
-
 			/* Write output file*/
 			FileWriter fileWriter;
 			try {
@@ -61,41 +51,52 @@ public class Main {
 				fileWriter.write(dotFile.toString());
 				fileWriter.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			// TODO add your handling code here:
-			//Process p = Runtime.getRuntime().exec("dot -T jpg -o graph.jpg graph.txt");
+			
+			// Store edges set for compute CDG
+			Set<Edge> edges = graph.edgeSet();
+			System.out.println("Edges : ");
+			for (Edge e : edges){
+				System.out.println(e);
+			}
+						
+			// Print graph
+			System.out.println("\n	Graph : \n");
+			System.out.println("Vertex : "+graph.vertexSet().toString());
+			System.out.println("Edges : "+edges);
+			System.out.println(graph.toString());
+			
+			// Run dot program to generate image and show it
 			Process p = Runtime.getRuntime().exec("/opt/local/bin/dot -T jpg -o graph.jpg graph.txt");
 	    	InputStream in = p.getInputStream();
 	    	InputStreamReader inread = new InputStreamReader(in);
 	    	BufferedReader bufferedreader = new BufferedReader(inread);
-	    	
 	    	// Check for failure
 			if (p.waitFor() != 0) {
 				System.out.println("exit value = " + p.exitValue());
 			}
-			System.out.println("Salida : "+bufferedreader.readLine());
+			System.out.println("outpud dot program : "+bufferedreader.readLine());
 			p = Runtime.getRuntime().exec("open graph.jpg");
 			
 			// Compute Dominators and Post Dominators and show
 			AlgorithmsDominators algorithmsDominator = new AlgorithmsDominators ();
-			
 			algorithmsDominator.computeDominators(graph);
-			
-			
 			// Compute Dominator
 			System.out.println("DOMINADORES:");
 			for (Vertex v : graph.vertexSet()) {
 				System.out.println("Dom("+v.toString()+")="+v.getDominators());
 			}
-			
 			// Compute Post-Dominator
 			SimpleDirectedGraph<Vertex,Edge> reverseGraph = algorithmsDominator.reverse(graph);
 			algorithmsDominator.computeDominators(reverseGraph);
 			System.out.println("POST-DOMINADORES:");
 			for (Vertex v : reverseGraph.vertexSet()) {
+				System.out.println("Dom("+v.toString()+")="+v.getDominators());
+			}			
+			System.out.println("DOMINADORES:");
+			for (Vertex v : graph.vertexSet()) {
 				System.out.println("Dom("+v.toString()+")="+v.getDominators());
 			}
 			
@@ -105,11 +106,9 @@ public class Main {
 				System.out.println("iPostDom("+v.toString()+")="+v.getiDominators());
 			}
 			
-			algorithmsDominator.computeDominatorsTree(reverseGraph);
-			
+			// Compute post dominators tree and write dot file
+			SimpleDirectedGraph<Vertex,Edge> dominatorsTree = algorithmsDominator.computeDominatorsTree(reverseGraph);
 			StringBuffer treeFile = algorithmsDominator.getOutputDot();
-			
-			/* Write output file*/
 			try {
 				fileWriter = new FileWriter("tree.txt");
 				fileWriter.write(treeFile.toString());
@@ -118,23 +117,28 @@ public class Main {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			// TODO add your handling code here:
-			//Process p = Runtime.getRuntime().exec("dot -T jpg -o graph.jpg graph.txt");
+			// Run dot program to generate image and show it
 			p = Runtime.getRuntime().exec("/opt/local/bin/dot -T jpg -o tree.jpg tree.txt");
 	    	in = p.getInputStream();
 	    	inread = new InputStreamReader(in);
 	    	bufferedreader = new BufferedReader(inread);
-	    	
 	    	// Check for failure
 			if (p.waitFor() != 0) {
 				System.out.println("exit value = " + p.exitValue());
 			}
-			System.out.println("Salida : "+bufferedreader.readLine());
+			System.out.println("outpud dot program : "+bufferedreader.readLine());
 			p = Runtime.getRuntime().exec("open tree.jpg");
 			
-			
-			
+			//ConnectivityInspector<Vertex,Edge> inspector = new ConnectivityInspector<Vertex,Edge>(dominatorsTree);
+			ConnectivityInspector<Vertex,Edge> inspector = new ConnectivityInspector<Vertex,Edge>(graph);
+			for (Edge<Vertex> e : edges){
+				if(!inspector.pathExists((Vertex)e.getTarget(), (Vertex)e.getSource())){
+					System.out.println("No es ansestro"+ e);
+				}else{
+					System.out.println("Es ansestro" + e);
+				}
+			}
+			SimpleDirectedGraph<Vertex,Edge> cdg = algorithmsDominator.computeControlDependenceGraph(graph, graph);
 			
 			
 			
